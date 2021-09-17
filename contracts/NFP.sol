b2ffebedb94133c8400d5713db05f62c8c59e22e
Guillaume Felley <guillaume@felley.io>
2021-09-17 20:36:37 +0200

    Constructor

--- contracts/NFP.sol
+++ contracts/NFP.sol
@@ -3,34 +3,28 @@
 pragma solidity >=0.6.2 <0.8.0;
 pragma abicoder v2;
 
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721BurnableUpgradeable.sol";
-import "./lazy-mint/ERC721Lazy.sol";
-import "./utils/HasContractURI.sol";
+import "./ERC721Base.sol";
 
-contract NFP is OwnableUpgradeable, ERC721BurnableUpgradeable, ERC721Lazy, HasContractURI {
+contract NFP is ERC721Base {
 
-    event CreateERC721RaribleUser(address owner, string name, string symbol);
+    event CreateERC721Rarible(address owner, string name, string symbol);
     
-    constructor(address[] memory operators) public {
-        __ERC721RaribleUser_init("Non-Fungible People", "NFP", "ipfs:/", "", operators);
+    constructor() public {
+        __ERC721Rarible_init("Non-Fungible People", "NFP", "ipfs:/", "");
     }
 
-    function __ERC721RaribleUser_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI, address[] memory operators) internal initializer {
+    function __ERC721Rarible_init(string memory _name, string memory _symbol, string memory baseURI, string memory contractURI) internal initializer {
         _setBaseURI(baseURI);
         __ERC721Lazy_init_unchained();
+        __RoyaltiesV2Upgradeable_init_unchained();
         __Context_init_unchained();
         __ERC165_init_unchained();
         __Ownable_init_unchained();
         __ERC721Burnable_init_unchained();
         __Mint721Validator_init_unchained();
         __HasContractURI_init_unchained(contractURI);
-        __RoyaltiesV2Upgradeable_init_unchained();
         __ERC721_init_unchained(_name, _symbol);
-        for(uint i = 0; i < operators.length; i++) {
-            setApprovalForAll(operators[i], true);
-        }
-        emit CreateERC721RaribleUser(_msgSender(), _name, _symbol);
+        emit CreateERC721Rarible(_msgSender(), _name, _symbol);
     }
 
     function mintAndTransfer(LibERC721LazyMint.Mint721Data memory data, address to) public override virtual {
